c6b9a979fd46682f6bd99c60240466db3313ac56
NikolayPostanogov <postanogov@nwtn-group.com>
2022-05-28 15:44:03 +0500

    added simple tests

---
+++ src/test/java/guru/qa/simple/NegativeTest.java
@@ -0,0 +1,28 @@
+package guru.qa.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTest {
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+    @Test
+    void test01() {
+        assertTrue( false);
+    }
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+    @Test
+    void test04() {
+        assertTrue(false);
+    }
+}
